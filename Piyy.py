c8c2e3c1b769090e03b86c7a41fc5ccf956da046
thebookcheese <jasperthebook@gmail.com>
2024-03-19 14:11:52 +0000

    Da fuq is

---
+++ Piyy.py
@@ -0,0 +1,8 @@
+class Pitta():
+    def __init__(self):
+        self.health = 60
+        self.attack = 5
+        self.defense = 1
+        self.attackboost = 0
+        self.defenseboost = 0 
+        self.regen = 1
