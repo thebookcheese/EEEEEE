c08edd25065da045c12817f1eb2b182bcc1ee998
thebookcheese <jasperthebook@gmail.com>
2024-03-22 08:32:59 +0000

    ADDed FIGht For SLIme

--- Fors.py
+++ Fors.py
@@ -9,3 +9,23 @@ def SlimeFight(p):
     SAtt = random.randint(2,3)
     SDef = random.randint(0,1)
     SDodge = random.randint(2,5)
+    
+    print("You encounter a slime with ",SHP," health")
+    while SHP > 0:
+        if p.health > 0:
+            Do = input("What do you want to do? \n Attack (1)")
+            if Do == "1":
+                SDamage = SAtt - p.health
+                Dodge = random.randint(1,100)
+                if Dodge < (p.dodge + 1):
+                    print("Player dodged")
+                else:
+                    p.health = p.health - SDamage
+                pdamage = p.attack - SDef
+                if Dodge < (SDodge + 1):
+                    print("Slime dodged")
+                else:
+                    SHP = SHP - pdamage
+            else:
+                print("NO")
+
