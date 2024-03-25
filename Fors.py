9eac60bbd632af816de716a72487468e322cd238
thebookcheese <jasperthebook@gmail.com>
2024-03-25 11:23:01 +0000

    AAAAAAAAA

--- Fors.py
+++ Fors.py
@@ -1,7 +1,25 @@
 import random
 
-def Drop1(p):
-    pass
+def Drop1(p, Min, Max):
+    Bonus = random.randint(Min,Max)
+    print("You found a sword which gives +",Bonus," attack")
+    Equip = input("Do you want to equip Y/N").lower()
+    if Equip == "y":
+        p.attackboost = p.attackboost + Bonus
+        print("Your attack bonus is +",p.attackboost)
+    else:
+        print("OK")
+
+def Drop2(p, Min, Max):
+    Bonus = random.randint(Min,Max)
+    print("You found a shield which gives +",Bonus," defense")
+    Equip = input("Do you want to equip Y/N").lower()
+    if Equip == "y":
+        p.defenseboost = p.defenseboost + Bonus
+        print("Your defense bonus is +",p.defenseboost)
+    else:
+        print("OK")
+
 
 def ForesMain(p):
     print("You in Fors")
@@ -19,14 +37,14 @@ def SlimeFight(p):
         if p.health > 0:
             Do = input("What do you want to do? \n Attack (1)")
             if Do == "1":
-                SDamage = SAtt - p.health
+                SDamage = SAtt - (p.health + p.defenseboost)
                 Dodge = random.randint(1,100)
                 if Dodge < (p.dodge + 1):
                     print("Player dodged")
                 else:
                     p.health = p.health - SDamage
                     print("You took ",SDamage," damage")
-                pdamage = p.attack - SDef
+                pdamage = (p.attack + p.attackboost) - SDef
                 if Dodge < (SDodge + 1):
                     print("Slime dodged")
                 else:
@@ -37,6 +55,13 @@ def SlimeFight(p):
         else:
             print("You died")
     else:
+        Loot = random.randint(1,2)
+        if Loot == 1:
+            Drop1(p,1,3)
+        else:
+            Drop2(p,1,3)
+        
+        
         print("Do you want to go back?")
         print("Yes")
 
